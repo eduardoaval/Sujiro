deeaa0b7aab8b5fcff9a784b027c917a5ec5dd8b
Eduardo Abreu <eduardoabreu09@gmail.com>
2025-04-15 22:50:58 -0300

    Add env variables

--- src/database/config/database.js
+++ src/database/config/database.js
@@ -1,10 +1,14 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
 module.exports = {
   dialect: "postgres",
-  host: "localhost",
-  username: "postgres",
-  password: "@Eduardo2025",
-  database: "Sujiro",
-  port: 5432,
+
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   define: {
     timestamps: true,
   },
